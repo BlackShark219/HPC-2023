65d005855a9b7b8080899dd0856b2aa7916d7697
BlackShark219 <71227986+BlackShark219@users.noreply.github.com>
2023-06-13 21:06:19 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-n=$(pbsnodes -x | sed 's/<Node>/\n/g' | grep -o '<name>.*</name>' | wc -l)
+n=$(pbsnodes -a | awk '/^s/{count++} END{print count}')
 echo Number of nodes: $n
-# Count the number of nodes with the same number of cores
 echo "Nodes with the same number of cores:"
 pbsnodes -a | awk '/np =/{print $3}' | sort | uniq -c | awk 
 '{printf "%d nodes with %d cores\n", $1, $2}'
-# Count the number of nodes with the same amount of memory
 echo "Nodes with the same amount of memory:"
 pbsnodes -a | awk '/physmem =/{print $10}' | sort | uniq -c | awk
 '{printf "%d nodes with %d GB of memory\n", $1, $2/$3}'

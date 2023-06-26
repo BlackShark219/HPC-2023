072d2be77eddcd6d01f5611d469d757910535666
BlackShark219 <71227986+BlackShark219@users.noreply.github.com>
2023-06-26 13:19:30 +0300

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -3,19 +3,12 @@ num_nodes=$(pbsnodes -a | awk '/^s/{count++} END{print count}')
 echo "Number of nodes in the cluster: $num_nodes"
 # Count the number of nodes with the same number of cores
 echo "Nodes with the same number of cores:"
-pbsnodes -a | awk '/np =/{print $3}' | sort | uniq -c | awk '{printf
-"%d nodes with %d cores\n", $1, $2}'
+pbsnodes -a | awk '/np =/{print $3}' | sort | uniq -c | awk '{printf "%d nodes with %d cores\n", $1, $2}'
 # Count the number of nodes with the same amount of memory
 echo "Nodes with the same amount of memory:"
-pbsnodes -a | awk '/physmem =/{print $10}' | sort | uniq -c | awk
-'{printf "%d nodes with %d GB of memory\n", $1, $2/$3}'
+pbsnodes -a | awk '/physmem =/{print $10}' | sort | uniq -c | awk '{printf "%d nodes with %d GB of memory\n", $1, $2/$3}'
 # Calculate the average amount of memory per core
-n_p=$(pbsnodes -x | sed 's/<Node>/\n/g' | grep -o '<np>.*</np>' | cut
--d'>' -f2 | cut -d'<' -f1 | tr '\n' ' ' | awk '{sum = 0; for (i=1;
-i<=NF; i++) {sum+=$i} print sum}')
-tot_mem_gb=$(pbsnodes -x | sed 's/<Node>/\n/g' | grep -o
-'<status>.*</status>' | grep -o 'totmem=.*' | cut -d',' -f1 | cut -d'='
--f2 | sed 's/kb//g' | tr '\n' ' ' | awk '{sum = 0; for (i=1; i<=NF;
-i++) {sum+=$i}; print sum/(1024*1024)}')
+n_p=$(pbsnodes -x | sed 's/<Node>/\n/g' | grep -o '<np>.*</np>' | cut -d'>' -f2 | cut -d'<' -f1 | tr '\n' ' ' | awk '{sum = 0; for (i=1; i<=NF; i++) {sum+=$i} print sum}')
+tot_mem_gb=$(pbsnodes -x | sed 's/<Node>/\n/g' | grep -o '<status>.*</status>' | grep -o 'totmem=.*' | cut -d',' -f1 | cut -d'='-f2 | sed 's/kb//g' | tr '\n' ' ' | awk '{sum = 0; for (i=1; i<=NF; i++) {sum+=$i}; print sum/(1024*1024)}')
 mem_per_proc=$(echo $tot_mem_gb $n_p | awk '{print $1/$2}')
 echo "The average amount of memory per proc is $mem_per_proc GB"

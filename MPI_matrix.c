856d3fa3724eb49a8ca790f957c7e848ede6e725
BlackShark219 <71227986+BlackShark219@users.noreply.github.com>
2023-06-28 16:37:36 +0300

    Update MPI_matrix.c

--- MPI_matrix.c
+++ MPI_matrix.c
@@ -8,23 +8,23 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-    int numRows = 8; // number of rows
-    int numCols = 12; // number of columns
-    int rowsPerProcess = 3;  // number of rows per process
+    int numRows = 10; // кількість рядків
+    int numCols = 20; // кількість стовпців
+    int rowsPerProcess = 2;  // кількість рядків на процес
 
-    // matrix initialization
+    // ініціалізація матриці
     double* matrix = malloc(sizeof(double) * numRows * numCols);
     int i;
     for (i = 0; i < numRows * numCols; i++) {
         matrix[i] = (double) rand() / RAND_MAX;
     }
 
-    // divide matrix per process
+    // розподіл матриці між процесами
     int count = rowsPerProcess * numCols;
     double* localMatrix = malloc(sizeof(double) * count);
     MPI_Scatter(matrix, count, MPI_DOUBLE, localMatrix, count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // search max element per part
+    // пошук максимального елемента в кожній частині
     double localMax = localMatrix[0];
     for (i = 0; i < rowsPerProcess * numCols; i++) {
         if (localMatrix[i] > localMax) {
@@ -32,12 +32,12 @@ int main(int argc, char** argv) {
         }
     }
 
-    // max element global
+    // глобальний максимальний елемент
     double globalMax;
     MPI_Reduce(&localMax, &globalMax, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
     if (processRank == 0) {
-        // print generated matrix
+        // виведення початкової матриці
         printf("Initialized matrix: \n");
         for (i = 0; i < numRows * numCols; i++) {
             printf("%f ", matrix[i]);
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
                 printf("\n");
             }
         }
-        // print global max element
+        // виведення глобального максимального елемента
         printf("Maximum element: %f\n", globalMax);
     }
 
